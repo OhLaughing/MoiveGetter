0456eb54df63feb53e3e861020b6f4d685558f67
bigwanggang <bigwanggang@163.com>
2018-03-12 10:27:18 +0800

    Update App.java

--- src/main/java/com/bigwanggang/App.java
+++ src/main/java/com/bigwanggang/App.java
@@ -1,14 +1,31 @@
 package com.bigwanggang;
 
+import cn.wanghaomiao.xpath.exception.XpathSyntaxErrorException;
+import cn.wanghaomiao.xpath.model.JXDocument;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 /**
  * Hello world!
  */
 public class App {
-    public static void main(String[] args) {
-        String s = "王牌保安][BD-mkv.720p.中英双字][2017年动作] ◎译　　名　王牌保安/保安 ◎片　　名　Security ◎年　　代　2017 ◎产　　地　美国 ◎类　　别　动作 ◎语　　言　英语 ◎字　　幕　中英双字幕 ◎IMDb评分  5.7/10 from 2,951 users ◎文件格式　x264 + AAC ◎视频尺寸　1280 x 720 ◎文件大小　1CD ◎片　　长　87分钟 ◎导　　演　Alain Desrochers ◎主　　演　本·金斯利 Ben Kingsley 　　　　　　安东尼奥·班德拉斯 Antonio Banderas 　　　　　　连姆·麦肯泰尔 Liam McIntyre 　　　　　　汪东城  Jiro Wang 　　　　　　查德·林德伯格 Chad Lindberg 　　　　　　加布里埃拉·赖特 Gabriella Wright 　　　　　　黎烈弓 Cung Le 　　　　　　马克·史密斯 Mark Rhino Smith 　　　　　　巴沙尔·拉海尔 Bashar Rahal ◎简　　介 　　退伍老兵(安东尼奥·班德拉斯饰)在一个购物商场当保安，然而他工作的首晚，一个被歹徒追击的女孩跑到商场寻求庇护……";
-        String[] ss = s.split("◎");
-        for (String s1 : ss) {
-            System.out.println(s1);
-        }
+    public static void main(String[] args) throws XpathSyntaxErrorException, IOException {
+
+
+        Document document = Jsoup.parse(new File("d:\\workspace\\ideaCode\\crawler-master\\3.html"), "gb2312");
+
+        System.out.println();
+        String a = "//*[@id=\"header\"]/div/div[3]/div[3]/div[2]/div[1]/div[1]/form/span/a";
+        JXDocument jxDocument = new JXDocument(document);
+//        List<Object> rs = jxDocument.sel("//*[@id="Zoom"]/span/p[5]/font[9]");
+        List<Object> rs = jxDocument.sel("//div[@id=Zoom]");
+        System.out.println(rs.size());
+        Element e = (Element) rs.get(0);
+        System.out.println(e.text().replace("【", "\n"));
     }
 }
